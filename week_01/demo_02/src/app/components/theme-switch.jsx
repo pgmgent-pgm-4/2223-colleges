d32694a6a5ac5f208ff577b30b5db3997e5347d1
Philippe De Pauw - Waterschoot <phildp@MACINTOSH.local>
2023-04-21 11:43:36 +0200

    Added conditional rendering to button theme switch

--- week_01/demo_02/src/app/components/theme-switch.jsx
+++ week_01/demo_02/src/app/components/theme-switch.jsx
@@ -4,7 +4,7 @@ const ThemeSwitch = () => {
   const [isDarkMode, setIsDarkMode] = useState(false);
 
   return (
-    <button className={``} onClick={() => setIsDarkMode(!isDarkMode)}>{isDarkMode ? 'Light' : 'Dark'} Mode</button>
+    <button className={``} style={ { backgroundColor: `${isDarkMode ? '#000' : '#fff'}`}} onClick={() => setIsDarkMode(!isDarkMode)}>{isDarkMode ? 'Light' : 'Dark'} Mode</button>
   );
 };
 
