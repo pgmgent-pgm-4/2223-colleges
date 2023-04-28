9c5bffeb1812c699de4fa3d48f56d0ab4fe9642a
Philippe De Pauw - Waterschoot <phildp@MACINTOSH.local>
2023-04-28 09:56:30 +0200

    Added Lifting State Up

--- week_02/demo_03/src/app/App.js
+++ week_02/demo_03/src/app/App.js
@@ -1,17 +1,27 @@
+// Import external modules
+import { useState } from 'react';
+
 // Import custom modules
 import { Clock, ThemeMode } from './components';
 import { TagCloud } from './components/tags';
+import { GuestGreeting, Mailbox, UserGreeting } from './components/hello';
 
 // Data
 import tagCloudData from './data/tag-cloud.json';
 
 // Styling
 import './App.css';
+import { TemperatureCalculator } from './components/temperature';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <div className="App">
       <TagCloud data={tagCloudData} />
+      { isLoggedIn ? <UserGreeting/> : <GuestGreeting />}
+      <Mailbox unreadMessages={100} />
+      <TemperatureCalculator />
       {/* <ThemeMode />
       <Clock city={`Ghent`} utc={1} />
       <Clock city={`New York`} utc={-6} /> */}
