ce9b6aaaf93fef133b020fe6c6e83a2fab83fdf7
Philippe De Pauw - Waterschoot <phildp@MACINTOSH.local>
2023-05-05 14:14:01 +0200

    Added demo_04

--- week_03/demo_04/src/app/components/makeup/makeup-product.js
+++ week_03/demo_04/src/app/components/makeup/makeup-product.js
@@ -1,13 +1,18 @@
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMoneyBill1, faStar, beatFade } from '@fortawesome/free-solid-svg-icons';
 
 const MakeupProduct = ({className, product}) => {
   return (
     <div className={`${className} card`}>
       <img src={product.image_link ? product.image_link : 'image.pnghttps://png.pngtree.com/png-vector/20210604/ourmid/pngtree-gray-network-placeholder-png-image_3416659.jpg'} className="card-img-top" alt="..." />
       <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <h5 className="card-title">{product.name}</h5>
+        <p className="card-text">{product.description}</p>
         <a href="#" className="btn btn-primary">Go somewhere</a>
+      </div>
+      <div className="card-body">
+        <span><FontAwesomeIcon icon={faMoneyBill1} beatFade /> {product.price}</span>
+        <span><FontAwesomeIcon icon={faStar} beatFade /> {product.rating}</span>
       </div>
     </div>
   )
